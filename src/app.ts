69a62dc66211f668e8642e2cf427c794d3e14f64
n-korel <n.korelskiy@hotmail.com>
2024-10-13 12:25:00 +0300

    11 - Первый шаг к архитектуре

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
-import { userRouter } from './users/users';
 import { LoggerService } from './logger/logger.service';
+import { UserController } from './users/users.controller';
+import { ExeptionFilter } from './errors/exeption.filter';
 
 
 export class App {
@@ -9,20 +10,33 @@ export class App {
 	server: Server;
 	port: number;
 	logger: LoggerService;
+	UserController: UserController;
+	exeptionFilter: ExeptionFilter;
 
-	constructor(logger: LoggerService) {
+	constructor(
+		logger: LoggerService,
+		UserController: UserController,
+		exeptionFilter: ExeptionFilter
+	) {
 		this.app = express();
 		this.port = 8000;
 		this.logger = logger;
+		this.UserController = UserController;
+		this.exeptionFilter = exeptionFilter;
 	}
 
 	useRoutes() {
-		this.app.use('/users', userRouter);
+		this.app.use('/users', this.UserController.router);
+	}
+
+	useExeptionFilters(){
+		this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
 	}
 
 	public async init() {
 		this.useRoutes();
+		this.useExeptionFilters();
 		this.server = this.app.listen(this.port);
-		this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
+		this.logger.log(`Сервер запущен на ${this.port}`);
 	}
 }
