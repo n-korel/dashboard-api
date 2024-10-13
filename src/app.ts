f793301b97e4f8a3500cf8d04beb135aecfe9094
n-korel <n.korelskiy@hotmail.com>
2024-10-14 00:21:43 +0300

    12 - Dependency Injection

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,29 @@
 import express, { Express } from 'express';
 import { Server } from 'http';
-import { LoggerService } from './logger/logger.service';
 import { UserController } from './users/users.controller';
 import { ExeptionFilter } from './errors/exeption.filter';
+import { ILogger } from './logger/logger.interface';
+import { inject, injectable } from 'inversify';
+import { TYPES } from './types';
+import 'reflect-metadata';
 
-
+@injectable()
 export class App {
 	app: Express;
 	server: Server;
 	port: number;
-	logger: LoggerService;
-	UserController: UserController;
-	exeptionFilter: ExeptionFilter;
 
 	constructor(
-		logger: LoggerService,
-		UserController: UserController,
-		exeptionFilter: ExeptionFilter
+		@inject(TYPES.ILogger) private logger: ILogger,
+		@inject(TYPES.UserController) private userController: UserController,
+		@inject(TYPES.ExeptionFilter) private exeptionFilter: ExeptionFilter
 	) {
 		this.app = express();
 		this.port = 8000;
-		this.logger = logger;
-		this.UserController = UserController;
-		this.exeptionFilter = exeptionFilter;
 	}
 
 	useRoutes() {
-		this.app.use('/users', this.UserController.router);
+		this.app.use('/users', this.userController.router);
 	}
 
 	useExeptionFilters(){
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,27 @@
+import { Container, ContainerModule, interfaces } from "inversify";
 import { App } from "./app";
 import { ExeptionFilter } from "./errors/exeption.filter";
 import { LoggerService } from "./logger/logger.service";
 import { UserController } from "./users/users.controller";
+import { ILogger } from "./logger/logger.interface";
+import { TYPES } from "./types";
+import { IExeptionFilter } from "./errors/exeption.filter.interface";
+import { IUserController } from "./users/users.controller.interface";
 
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+	bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<App>(TYPES.Application).to(App);
+});
 
-async function bootstrap() {
-	const logger = new LoggerService();
-	const app = new App(
-		logger, 
-		new UserController(logger), 
-		new ExeptionFilter(logger)
-	);
-	await app.init();
+function bootstrap() {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	app.init();
+	return { app, appContainer };  
 }
 
-bootstrap();
+
+export const { app, appContainer } = bootstrap();
