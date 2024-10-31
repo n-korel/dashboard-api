0d8839c3648893f8b90ed1ba7f7b4203e74957ab
n-korel <n.korelskiy@hotmail.com>
2024-10-31 15:39:57 +0300

    17 - Тесты

--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -16,6 +16,35 @@ describe('Users e2e', () => {
 			.send({ email: 'pop@mad.com', password: '1' });
 		expect(res.statusCode).toBe(422);
 	});
+
+	it('Login - success', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'pop@mad.com', password: '123' });
+		expect(res.body.jwt).not.toBeUndefined();
+	});
+
+	it('Login - error', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'pop@mad.com', password: '1' });
+		expect(res.statusCode).toBe(401);
+	});
+
+	it('Info - success', async () => {
+		const login = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'pop@mad.com', password: '123' });
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${login.body.jwt}`);
+		expect(res.body.email).toBe('pop@mad.com');
+	});
+
+	it('Info - error', async () => {
+		const res = await request(application.app).get('/users/info').set('Authorization', `Bearer 1`);
+		expect(res.statusCode).toBe(401);
+	});
 });
 
 afterAll(() => {
